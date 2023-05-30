1594ef33c75b286068b32108287ce61a51af9323
Amenorach <77848293+Amenorach@users.noreply.github.com>
2023-05-30 21:00:14 +0000

    form

--- index.php
+++ index.php
@@ -6,6 +6,11 @@
     <title>Home</title>
 </head>
 <body>
-    
+    <h2>TODO</h2>
+    <form action="">
+        <input type="radio" name="createTodo" placeholder="Create a new todo" id="create">
+    </form>
+    <br>
+
 </body>
 </html>
