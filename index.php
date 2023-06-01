428894cbb0064f6677a5eb7eae2d9b5a4fd5a155
Amenorach <77848293+Amenorach@users.noreply.github.com>
2023-06-01 19:21:31 +0000

    try

--- index.php
+++ index.php
@@ -1,19 +1,23 @@
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>To-Do App</title>
-  <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>To-Do App</title>
+    <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-  <div class="container">
-    <h1>To-Do App</h1>
-    <input type="text" id="new-todo" placeholder="Enter a new to-do">
-    <button id="add-todo">Add</button>
-    <ul id="todos"></ul>
-  </div>
-  <script src="script.js"></script>
+    <div class="container">
+        <h1>TO DO</h1>
+        <div class="newTodo">
+            <input type="text" id="new-todo" placeholder="Enter a new to-do">
+            <button id="add-todo">Add</button>
+        </div>
+        <ul id="todos"></ul>
+    </div>
+    <script src="script.js"></script>
 </body>
+
 </html>
--- script.js
+++ script.js
@@ -1,8 +1,42 @@
 // This is the JavaScript for the to-do app. It adds interactivity to the app, such as adding new to-dos, marking to-dos as complete, and deleting to-dos.
-const todos = []; // This is an array that stores all of the to-dos.
+// const todos = []; // This is an array that stores all of the to-dos.
+
+// const addTodo = () => {
+//     // This function adds a new to-do to the list.
+//     const newTodo = document.getElementById("new-todo").value;
+//     todos.push({
+//         text: newTodo,
+//         completed: false,
+//     });
+//     refreshTodos();
+// };
+
+// const refreshTodos = () => {
+//     // This function refreshes the list of to-dos.
+//     const todoList = document.getElementById("todos");
+//     todoList.innerHTML = todos.map(todo => `
+//     <li data-id="${todo.id}">
+//       ${todo.text}
+//       <button class="complete">Complete</button>
+//     </li>
+//   `).join("");
+// };
+
+// const handleComplete = (event) => {
+//     // This function marks a to-do as complete.
+//     const todo = event.target.parentElement;
+//     todo.classList.toggle("completed");
+//     todo.dataset.completed = todo.classList.contains("completed");
+// };
+
+// document.getElementById("add-todo").addEventListener("click", addTodo);
+// document.querySelectorAll(".complete").forEach(button => button.addEventListener("click", handleComplete));
+
+// refreshTodos();
+
+const todos = [];
 
 const addTodo = () => {
-    // This function adds a new to-do to the list.
     const newTodo = document.getElementById("new-todo").value;
     todos.push({
         text: newTodo,
@@ -12,21 +46,33 @@ const addTodo = () => {
 };
 
 const refreshTodos = () => {
-    // This function refreshes the list of to-dos.
     const todoList = document.getElementById("todos");
     todoList.innerHTML = todos.map(todo => `
     <li data-id="${todo.id}">
       ${todo.text}
-      <button class="complete">Complete</button>
+      <ul>
+        <li>
+          <input type="radio" name="radio" id="radio1" ${todo.completed ? "checked" : ""}>
+          <label for="radio1">This is the first radio button</label>
+        </li>
+        <li>
+          <input type="radio" name="radio" id="radio2" ${todo.completed ? "" : "checked"}>
+          <label for="radio2">This is the second radio button</label>
+        </li>
+      </ul>
     </li>
   `).join("");
 };
 
 const handleComplete = (event) => {
-    // This function marks a to-do as complete.
     const todo = event.target.parentElement;
     todo.classList.toggle("completed");
     todo.dataset.completed = todo.classList.contains("completed");
+
+    // Remove the todo from the list if it is marked as completed
+    if (todo.classList.contains("completed")) {
+        todos.splice(todos.indexOf(todo), 1);
+    }
 };
 
 document.getElementById("add-todo").addEventListener("click", addTodo);
